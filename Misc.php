2f8b7e81eb7e20818974f25429d263bf08b0c343
Michael Angelo Calimlim <macalimlim@gmail.com>
2014-10-29 07:48:53 +0800

    refactor compose function

--- Misc.php
+++ Misc.php
@@ -4,8 +4,10 @@ class Misc {
     public static function id($x) {
         return $x;
     }
-    public static function compose($f, $g, $v) {
-        return $f($g($v));
+    public static function compose($f, $g) {
+        return function($v) use ($f, $g) {
+            return $f($g($v));
+        };
     }
     public static function appop($f, $v) {
         return $f($v);
